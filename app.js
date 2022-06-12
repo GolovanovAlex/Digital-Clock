13562b2cde3c24ef49c6a135ff02e0cab7bfd678
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-06-12 23:10:41 +0300

    js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 let hours = document.getElementById('hours');
 let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
+let ampm = document.getElementById('ampm');
+
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML = s;
