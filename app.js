5ce8dc88ff197ad3c2afd10caf5c438e29a7a252
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-06-13 07:55:26 +0300

    js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-let hours = document.getElementById('hours');
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
-let ampm = document.getElementById('ampm');
+setInterval(() => {
+  let hours = document.getElementById('hours');
+  let minutes = document.getElementById('minutes');
+  let seconds = document.getElementById('seconds');
+  let ampm = document.getElementById('ampm');
 
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSeconds();
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let am = h >= 12 ? 'PM' : 'AM';
 
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
+  // 24 to 12
+  if (h > 12) {
+    h = h - 12;
+  }
+  // add zero
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  ampm.innerHTML = am;
+});
